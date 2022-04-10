8322c6772ce02e9a76485b527ee9012a271b1abd
KapilDeoSharma <89604957+KapilDeoSharma@users.noreply.github.com>
2022-04-10 12:58:56 -0500

    init
    init

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,8 +1,15 @@
+# NeuralNet model for Autonomus Driving Vehicle
+
+
+# Importing required libraries
 import tflearn
 from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.estimator import regression
 from tflearn.layers.normalization import local_response_normalization
+
+
+# Defining model
 
 def neuralnet(width, height, lr):
     network = input_data(shape=[None, width, height, 1], name='input')
